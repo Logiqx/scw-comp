2762512590ee361fdb20a456504c00b5157cbb30
Logiqx <software@mikeg.me.uk>
2020-03-06 19:01:10 +0000

    Use Alpine Linux

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -6,7 +6,7 @@ IMAGE_NAME=$PROJ_NAME
 IMAGE_TAG=$(git rev-parse --short=12 HEAD)
 
 # Docker Build
-DOCKER_BUILDKIT=1 docker build . --file Dockerfile-slim --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
+DOCKER_BUILDKIT=1 docker build . --file Dockerfile --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
 
 # Refresh Competitions
 run_py_script Weekly_Competition.py
