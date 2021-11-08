b92eaaaad5ad3a461c0a8fb292ffe2c1472aa855
Logiqx <software@mikeg.me.uk>
2021-11-08 11:49:16 +0000

    Remove hard-coded image details from build script

--- bin/py_build.sh
+++ bin/py_build.sh
@@ -9,7 +9,7 @@ IMAGE_TAG=tmp
 DOCKER_BUILDKIT=1 docker build . --file Dockerfile --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
 
 # Copy the library scripts
-docker run --rm --entrypoint cat scw-comp:tmp $WORK_DIR/python/Common_Functions.py >$PROJ_DIR/python/Common_Functions.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/Common_Functions.py >$PROJ_DIR/python/Common_Functions.py
 
 # Clear pycache
 rm -f $PROJ_DIR/python/__pycache__/Common_Functions.*.pyc
